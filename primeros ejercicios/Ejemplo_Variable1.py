ee89ada394da8ed61efd2c0fae57db404c2c6698
tigreguapo <124531390+tigreguap0@users.noreply.github.com>
2023-09-22 09:31:24 +0200

    Add files via upload

---
+++ primeros ejercicios/Ejemplo_Variable1.py
@@ -0,0 +1,6 @@
+Var1= "Hola Mundo"
+print("La variable Var1 contiene el texto", Var1)
+Var1=4
+print("La variable Var1 contiene el numero", Var1)
+Var1=3.2
+print("La variable Var1 contiene un numero con decimales", Var1)
