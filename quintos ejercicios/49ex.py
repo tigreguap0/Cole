cb3ccc4e8eceac37a04a38fa1c7160ba41b360cd
tigreguapo <124531390+tigreguap0@users.noreply.github.com>
2023-11-02 09:45:25 +0100

    Add files via upload

--- quintos ejercicios/49ex.py
+++ quintos ejercicios/49ex.py
@@ -5,8 +5,12 @@ oportunidades = int(input("Introduce el número de oportunidades: "))
 for _ in range(oportunidades):
     letra = input("Introduce una letra: ")
     if letra in palabra_secreta:
-        print("La letra existe")
+        posiciones = [str(i) for i, c in enumerate(palabra_secreta) if c == letra]
+        if posiciones:
+            print("La letra se encuentra en la posición(es):", ", ".join(posiciones))
+        else:
+            print("La letra no existe en la palabra secreta.")
     else:
-        print("La letra no existe")
+        print("La letra no existe en la palabra secreta.")
 
 print("Se te han agotado las oportunidades. La palabra secreta era:", palabra_secreta)
