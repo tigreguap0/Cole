2107c2f5ce8f1302b38f8fb7e8caf3e36194a693
tigreguapo <124531390+tigreguap0@users.noreply.github.com>
2024-03-14 09:46:20 +0100

    Delete jutge directory

--- jutge/P37469.py
+++
@@ -1,9 +0,0 @@
-def convert_seconds_to_hms(total_seconds):
-    minutes, seconds = divmod(total_seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-
-    return hours, minutes, seconds
-
-total_seconds = int(input())
-hours, minutes, seconds = convert_seconds_to_hms(total_seconds)
-print(f"{hours} {minutes} {seconds}")
--- jutge/P92613.py
+++
@@ -1,9 +0,0 @@
-import math
-
-x = float(input())
-
-floor_x = math.floor(x)
-ceiling_x = math.ceil(x)
-rounded_x = round(x)
-
-print(floor_x, ceiling_x, rounded_x)
