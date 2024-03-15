904d468998d830213ff5f6e6270b6fe232381f3b
tigreguapo <124531390+tigreguap0@users.noreply.github.com>
2024-03-15 09:33:47 +0100

    Delete jutge directory

--- jutge/P56118.py
+++
@@ -1,8 +0,0 @@
-# Read two integers from input
-num1, num2 = map(int, input().split())
-
-# Find the maximum of the two numbers
-max_num = max(num1, num2)
-
-# Print the maximum
-print(max_num)  
--- jutge/P98960.py
+++
@@ -1,5 +0,0 @@
-palabra = input("")
-if palabra.isupper():
-    print(palabra.lower())
-else:
-    print(palabra.upper())
